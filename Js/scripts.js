2b9ccb20ae6e558c5201dcffad5ae21785feb8e3
Danlon Situma <situmadanlon@gmail.com>
2021-02-22 14:12:30 +0300

    Add Akan names

--- Js/scripts.js
+++ Js/scripts.js
@@ -3,4 +3,11 @@ const getName=() => {
     let date=document.getElementById("birthday").value 
  
     console.log(date)
+    //listing days of the week
+    const daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    console.log(daysOfTheWeek[dayBorn])
+    // Akan names arrays
+    const male =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+
+    const female = ["Akosua","Adwao","Abenaa","Akua","Yaa","Afua","Ama"]
 }
